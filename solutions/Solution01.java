825748bb42bc716c4549b553082561cb6dacf1c1
Pradeep Kumar Tippa <pktippa@gmail.com>
2018-06-07 16:30:58 +0530

    Added Solution for 01 exercie

---
+++ solutions/Solution01.java
@@ -0,0 +1,33 @@
+class Solution01{
+  public static void main(String args[]){
+    
+    //whole numbers
+    short num01 = 152;
+    short num03 = 32500;
+    short num04 = 25612;
+    int num05 = num03 + num04;
+    System.out.println("sum:"+num05);
+    
+    //decimals
+    
+    float num06 = 544.12f;
+    byte num07 = 15;
+    float num08 = num06 + num07;
+    
+    System.out.println("sum of decimals:"+num08);
+    
+    //Boolean
+    //character
+    char char01 = '\u03A3';
+    System.out.println("sigma symbol:"+char01);
+    
+    String fname = "bhavani";
+    char lname = 'T';
+    String fullname = fname + " " +lname;
+    System.out.println("fullname:"+fullname);
+    
+    boolean bool01 = true;
+    boolean bool02 = false;
+    //System.out.println(bool01 + bool02);
+  }
+}
